b72580b1ae05857126388e6d0cf80e86c00af09d
web3foryou <94456349+web3foryou@users.noreply.github.com>
2022-05-05 17:43:12 +0300

    upd

--- app/address.ts
+++ app/address.ts
@@ -26,11 +26,11 @@ export class Address {
             this.USER = process.env.ADDRESS_1 as string;
         } else if (network == "ganache2") {
             this.PLATFORM = process.env.CONTRACT_PLATFORM_G2 as string;
-            this.DAO = process.env.CONTRACT_DAO_RINKEBY_G2 as string;
-            this.STAKING = process.env.CONTRACT_STAKING_RINKEBY_G2 as string;
-            this.ACDM = process.env.CONTRACT_ACDM_RINKEBY_G2 as string;
-            this.LP = process.env.CONTRACT_LP_RINKEBY_G2 as string;
-            this.XXX = process.env.CONTRACT_XXX_RINKEBY_G2 as string;
+            this.DAO = process.env.CONTRACT_DAO_G2 as string;
+            this.STAKING = process.env.CONTRACT_STAKING_G2 as string;
+            this.ACDM = process.env.CONTRACT_ACDM_G2 as string;
+            this.LP = process.env.CONTRACT_LP_G2 as string;
+            this.XXX = process.env.CONTRACT_XXX_G2 as string;
             this.USER = process.env.ADDRESS_1_G2 as string;
         }
     }
--- tasks/uniswapLp/uniswapLpBalanceOf.ts
+++ tasks/uniswapLp/uniswapLpBalanceOf.ts
@@ -9,13 +9,12 @@ task("uniswapLpBalanceOf", "uniswapLpBalanceOf")
         let addresses = new Address(process.env.NETWORK as string);
 
         const lpArtifact = require('../../artifacts/contracts/LpToken.sol/LpToken.json');
-        let lp = new hre.ethers.Contract(addresses.LP_TEST, lpArtifact.abi, signer);
+        let lp = new hre.ethers.Contract(addresses.LP, lpArtifact.abi, signer);
         let lpSigner = lp.connect(signer);
 
         let balanceOf = await lpSigner.balanceOf(signer.address);
 
-        console.log("NETWORK: " + process.env.NETWORK as string);
-        console.log("address: " + signer.address);
+        console.log("address: " + await signer.address);
         console.log("balanceOf: " + balanceOf);
         console.log("Done");
     });
